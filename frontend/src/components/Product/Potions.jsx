6ea185bb3fbdeb5f58233fd5c1715045a6f718a8
Islaster <52678788+Islaster@users.noreply.github.com>
2022-08-26 17:19:56 -0700

    set up simple fullstack & potion data

---
+++ frontend/src/components/Product/Potions.jsx
@@ -0,0 +1,297 @@
+export const Data = {
+  Health: [
+    {
+      //RED POTION
+      Name: "Minor Potion",
+      effects: "40 health",
+      Price: 10,
+      src: "https://static.wikia.nocookie.net/empiresandpuzzles/images/c/c1/Minor_healing_potion_icon.png/revision/latest?cb=20190116033149",
+    },
+    {
+      name: "Lesser Potion",
+      effects: "60 health",
+      price: 15,
+      src: "https://static.wikia.nocookie.net/empiresandpuzzles/images/c/c1/Minor_healing_potion_icon.png/revision/latest?cb=20190116033149",
+    },
+    {
+      name: "Greater Potion",
+      effects: "80 health",
+      price: 25,
+      src: "https://db4sgowjqfwig.cloudfront.net/images/4490396/Potion_of_Superior_Healing.png",
+    },
+    {
+      name: "Major Potion",
+      effects: "100 health",
+      price: 30,
+      src: "https://db4sgowjqfwig.cloudfront.net/images/4490396/Potion_of_Superior_Healing.png",
+    },
+    {
+      name: "Enhanced Minor Potion",
+      effects: "120 health",
+      price: 40,
+      src: "https://static.wikia.nocookie.net/skyrim_gamepedia/images/0/0a/PotionofMinorHealing.png/revision/latest?cb=20111221191747",
+    },
+    {
+      name: "Enhanced Lesser Potion",
+      effects: "140 health",
+      price: 45,
+      src: "https://static.wikia.nocookie.net/skyrim_gamepedia/images/0/0a/PotionofMinorHealing.png/revision/latest?cb=20111221191747",
+    },
+    {
+      name: "Enhanced Greater Potion",
+      effects: "160 health",
+      price: 50,
+      src: "https://cdna.artstation.com/p/assets/images/images/011/393/530/large/daniel-humpage-greater-healing-potion.jpg?1529366646",
+    },
+    {
+      name: "Enhanced Major Potion",
+      effects: "180 health",
+      price: 55,
+      src: "https://cdna.artstation.com/p/assets/images/images/011/393/530/large/daniel-humpage-greater-healing-potion.jpg?1529366646",
+    },
+    {
+      name: "Greater Flask",
+      effects: "220 health",
+      price: 60,
+      src: "https://ih1.redbubble.net/image.1203151870.8185/st,small,507x507-pad,600x600,f8f8f8.jpg",
+    },
+    {
+      name: "Major Flask",
+      effects: "260 health",
+      price: 65,
+      src: "https://ih1.redbubble.net/image.1203151870.8185/st,small,507x507-pad,600x600,f8f8f8.jpg",
+    },
+    {
+      name: "Mega Potion",
+      effect: "500 health",
+      price: 100,
+      src: "https://cdn1.epicgames.com/ue/product/Screenshot/Render5-1920x1080-74582b776f64e2a548ed9bc47cbd4d1b.png?resize=1&w=1920",
+    },
+    {
+      name: "Max Potion",
+      effects: "2500 health",
+      price: 250,
+      src: "https://static.wikia.nocookie.net/pokemongo_gamepedia_en/images/0/03/Max_Potion.png/revision/latest?cb=20160714140817",
+    },
+  ],
+  boost: [
+    {
+      //ORANGE POTION
+      name: "Speed Boost",
+      effects: 10, //Percentage
+      price: 80,
+      duration: 5, //minutes
+      src: "https://ih1.redbubble.net/image.2918710353.4683/st,small,507x507-pad,600x600,f8f8f8.jpg",
+    },
+    {
+      name: "Enhanced Speed Boost",
+      effects: 20, //Percentage
+      price: 200,
+      duration: 10, //minutes
+      src: "https://i.pinimg.com/736x/b8/77/b3/b877b3872db9e844c2d9942a396e277c.jpg",
+    },
+    {
+      name: "Perfected Speed Boost",
+      effects: 50, //Percentage
+      price: 500,
+      duration: 25, //minutes
+      src: "https://static.wikia.nocookie.net/the-ash-princess/images/e/ec/57_1_1540857190.jpg/revision/latest?cb=20190725000247",
+    },
+    {
+      //PURPLE POTION
+      name: "XP Boost", //Experience
+      effects: 30, //Percentage
+      price: 80,
+      duration: 5,
+      src: "https://e7.pngegg.com/pngimages/698/740/png-clipart-clear-glass-bottle-filled-with-purple-liquid-potion-salem-witch-trials-magic-watercolor-purple-miscellaneous-game.png",
+    },
+    {
+      name: "Enchanced XP Boost", //Experience
+      effects: 60, //Percentage
+      price: 200,
+      duration: 10,
+      src: "https://i.pinimg.com/originals/97/34/c9/9734c90ce589906bd0fefe597ff42e5c.jpg",
+    },
+    {
+      name: "Perfected XP Boost", //Experience
+      effects: 100, //Percentage
+      price: 500,
+      duration: 25,
+      src: "https://i.pinimg.com/736x/94/ff/40/94ff40c7b1ca1a253e0af66ec70577a5--elixir-rpg-items.jpg",
+    },
+    {
+      //GREY
+      name: "ATK Boost", //Attack
+      effects: 5, //plus 5 ATK
+      price: 50,
+      duration: 3, //battles
+      src: "https://thumbs.dreamstime.com/b/potion-gray-liquid-alchemy-set-flask-small-glass-bottle-colored-game-role-play-magic-potions-cork-stopper-larp-bottles-194856833.jpg",
+    },
+    {
+      name: "Enhanced ATK Boost", //Attack
+      effects: 15, //plus 15 ATK
+      price: 150,
+      duration: 3, //battles
+      src: "https://thumbs.dreamstime.com/b/bottle-smoke-game-icon-magic-elixir-interface-rpg-match-game-smoke-clouds-big-variant-bottle-smoke-game-icon-100044724.jpg",
+    },
+    {
+      //DARK BLUE
+      name: "MATK Boost", // Magical Attack
+      effects: 5, //plus 5 MATK
+      price: 50,
+      duration: 3, //battles
+      src: "https://i.pinimg.com/736x/d5/e5/65/d5e565effe1cac9ffb498bc3358ad6d0.jpg",
+    },
+    {
+      name: "Enhanced MATK Boost", // Magical Attack
+      effects: 15, //plus 15 MATK
+      price: 150,
+      duration: 3, //battles
+      src: "https://i.pinimg.com/736x/ed/0a/76/ed0a769ce03e5c9055df407a70888b58--glow-egg.jpg",
+    },
+    {
+      //YELLOW
+      name: "DEF Boost", // Defense
+      effects: 5, //plus 5 DEF
+      price: 50,
+      duration: 3, //battles
+      src: "https://static.wikia.nocookie.net/roblox-dragon-blade-rpg/images/1/1d/Strength_Potion.png/revision/latest?cb=20220410144429",
+    },
+    {
+      name: "Enhanced DEF Boost", // Defense
+      effects: 15,
+      price: 150,
+      duration: 3,
+      src: "https://i.pinimg.com/564x/78/e8/49/78e84988a2fbda1b5e99d77221c32d9b.jpg",
+    },
+    {
+      //DARK YELLOW
+      name: "MDEF Boost", // Magical Defense
+      effects: 5,
+      price: 50,
+      duration: 3,
+      src: "https://thumbs.dreamstime.com/b/bottle-gold-game-icon-magic-elixir-interface-rpg-match-game-gold-small-variant-bottle-gold-game-icon-magic-100044958.jpg",
+    },
+    {
+      name: "Enhanced MDEF Boost", // Magical Defense
+      effects: 15,
+      price: 150,
+      duration: 3,
+      src: "https://thumbs.dreamstime.com/b/frame-wreath-magic-yellow-potion-square-bottle-vector-illustration-elixir-game-203499105.jpg",
+    },
+    {
+      //LIGHT GREEN
+      name: "ACC Boost", //accuracy
+      effects: 5,
+      price: 50,
+      duration: 3,
+      SRC: "https://thumbs.dreamstime.com/b/transparent-bottle-vibrant-green-liquid-isolated-dark-background-transparent-bottle-vibrant-green-liquid-112138122.jpg",
+    },
+    {
+      name: "Enhanced ACC Boost", //accuracy
+      effects: 10,
+      price: 100,
+      duration: 3,
+    },
+    {
+      name: "STR Boost", //strength
+      effects: 2, // plus 2 str
+      price: 50,
+      duartion: 3,
+    },
+    {
+      name: "Enhanced STR Boost", //strength
+      effects: 5, //plus 5 str
+      price: 100,
+      duration: 3,
+    },
+    {
+      name: "INT Boost", //intelligence
+      effects: 2,
+      price: 85,
+      duration: 3,
+    },
+    {
+      name: "Enhanced INT Boost", //intelligence
+      effects: 5,
+      price: 170,
+      duration: 3,
+    },
+    {
+      name: "DEX Boost", //dexterity
+      effects: 2,
+      price: 150,
+      duration: 3,
+    },
+    {
+      name: "Enhanced DEX Boost", //dexterity
+      effects: 5,
+      price: 350,
+      duration: 3,
+    },
+    {
+      name: "Witch's Brew", //Luck
+      effects: 6,
+      price: 200,
+      duration: 3,
+    },
+    {
+      name: "HP Boost", //health
+      effects: 100, //max health plus 100
+      price: 200,
+      duration: 3,
+    },
+  ],
+  enchantments: [
+    {
+      name: "ATK Enchant", //attack
+      effects: 3, //atk plus 3
+      price: 150,
+    },
+    {
+      name: "ATK PLV Enchant", //Attack per level
+      effects: 0.1,
+      price: 300,
+    },
+    {
+      name: "MATK Enchant", //magic attack
+      effects: 3,
+      price: 150,
+    },
+    {
+      name: "MATK PLV Enchant", //magic attack per level
+      effects: 0.1,
+      price: 300,
+    },
+    {
+      name: "DEF Enchant", //Defense
+      effects: 3,
+      price: 150,
+    },
+    {
+      name: "DEF PLV Enchant", //Defense per level
+      effects: 0.1,
+      price: 300,
+    },
+    {
+      name: "MDEF Enchant", //magic defense
+      effects: 3,
+      price: 150,
+    },
+    {
+      name: "MDEF PLV Enchant", //magic defense per level
+      effects: 0.1,
+      price: 300,
+    },
+    {
+      name: "ACC Enchant", //accuracy
+      effects: 3,
+      price: 150,
+    },
+    {
+      name: "ACC PLV Enchant", //accuracy per level
+      effects: 0.1,
+      price: 300,
+    },
+  ],
+};
